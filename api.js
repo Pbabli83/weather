5c9ab01b83b1c93cc45e450484a74fea4938d0e2
babli patel <bp0008922@gmail.com>
2023-03-22 00:22:15 +0530

    second commit

---
+++ api.js
@@ -0,0 +1,45 @@
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentweatherItemsEl = document.getElementById('current-weather-items');
+const timezone = document.getElementById('time-zone');
+const countryE1 = document.getElementById('country');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Suturday']
+const months = ['jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+const API_KEY =('0f9ccf04b63971b580b49b8f0c6f46c4')
+
+setInterval(() => {
+   const time = new Date();
+   const month = time.getMonth();
+   const date = time.getDate();
+   const day = time.getDay();
+   const hour = time.getHours();
+   const hoursIn12HrFormat = hour >= 13 ? hour % 12 : hour
+   const minutes = time.getMinutes();
+   const ampm = hour >= 12 ? 'PM' : 'AM'
+
+   timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes + `<span id="am-pm">${ampm}</span>`
+
+   dateEl.innerHTML = days[day] + ', ' + date + '' + months[month]
+
+}, 1000);
+
+ getWeatherData()
+ function getWeatherData() {
+    navigator.geolocation.getCurrentPosition((success) => {
+       console.log(success);
+
+      let{latitude, longitude } =success.coords;
+
+       fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${API_KEY}`).then(res => res.json()).then(data =>{
+          console.log(data)
+         //  showWeatherData(data);
+          
+       })
+    })
+
+ }
+
